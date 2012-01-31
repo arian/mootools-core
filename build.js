c95fcf35bbc75858665e7fbcfa7c86367dc9c2dc
Arian <arian@aryweb.nl>
2012-01-31 02:59:17 +0100

    Add build.js option to include all modules.

--- build.js
+++ build.js
@@ -26,6 +26,7 @@ var help = 'Usage: node build.js [options] <modules>\n'
 	+ ' --help                 Show this help message\n'
 	+ ' --optimize             Use to optimize the file with UglifyJS\n'
 	+ ' --has <key> <value>    Set key-values for has.js\n'
+	+ ' --all                  Includes all modules\n'
 	+ '\n';
 
 // array with modules we'll require
@@ -42,10 +43,54 @@ for (var i = 2; i < process.argv.length; i++){
 		optimize = true;
 	} else if (arg == '--has'){
 		has[process.argv[++i]] = process.argv[++i];
+	} else if (arg == '--all'){
+
+		[
+			'Core/Core',
+			'Types/Array',
+			'Types/String',
+			'Types/Number',
+			'Types/Function',
+			'Types/Object',
+			'Types/DOMEvent',
+			'Browser/Browser',
+			'Class/Class',
+			'Class/Class.Extras',
+			'Element/Element',
+			'Element/Element.Style',
+			'Element/Element.Event',
+			'Element/Element.Delegation',
+			'Element/Element.Dimensions',
+			'Fx/Fx',
+			'Fx/Fx.CSS',
+			'Fx/Fx.Tween',
+			'Fx/Fx.Morph',
+			'Fx/Fx.Transitions',
+			'Request/Request',
+			'Request/Request.HTML',
+			'Request/Request.JSON',
+			'Utilities/Cookie',
+			'Utilities/JSON',
+			'Utilities/DOMReady',
+			'Utilities/Swiff'
+		].forEach(function(file){
+			requires.push('MooTools/1/' + file);
+		});
+
+		requires.push('Slick/Slick.Parser');
+		requires.push('Slick/Slick.Finder');
+
 	} else {
 		requires.push(arg);
 	}
 }
+
+// make requires unique, we only have to care about strings, so we can use object keys.
+requires = (function(){
+	var o = {};
+	for (var i = requires.length; i--;) o[requires[i]] = 1;
+	return Object.keys(o);
+})();
 
 if (!requires.length){
 	log.red('\nYou did not select any modules!\n');
@@ -71,4 +116,3 @@ requirejs.optimize(config, function(buildResponse){
 	var contents = fs.readFileSync(config.out, 'utf8');
 	process.stdout.write(contents + '\n');
 });
-
